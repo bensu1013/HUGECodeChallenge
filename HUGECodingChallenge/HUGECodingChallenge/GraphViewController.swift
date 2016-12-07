82d1a4b3e6de2c1cb61e7400e0d618e19dfd5167
Ben Su <bensu1013@hotmail.com>
2016-12-07 18:07:14 -0500

    added stuff yay

--- HUGECodingChallenge/HUGECodingChallenge/GraphViewController.swift
+++ HUGECodingChallenge/HUGECodingChallenge/GraphViewController.swift
@@ -23,6 +23,12 @@ class GraphViewController: UIViewController {
     
     @IBOutlet weak var graphView: UIView!
     
+    let usdGraph = UIView()
+    let gbpGraph = UIView()
+    let eurGraph = UIView()
+    let jpyGraph = UIView()
+    let brlGraph = UIView()
+    
     var exchangeRates = [String : Double]()
     var exchangeAmounts = [String : Double]()
     var inputAmount: Double = 0
@@ -30,11 +36,13 @@ class GraphViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.exchangeAmounts = populateExchangeAmounts(rates: exchangeRates)
+        self.setupGraphSubviews()
         self.placeExchangeLabels()
         self.calculateForGraphLabels()
         
+        self.animateGraphView()
     }
     
     @IBAction func doneButtonAction(_ sender: UIButton) {
@@ -94,9 +102,9 @@ class GraphViewController: UIViewController {
     func calculateForGraphLabels() {
         
         highAmountLabel.text = "\(highestExchange)"
-        midAmountLabel.text = "\(changeDoubleToMoney(amount: highestExchange * 0.45))"
-        lowAmountLabel.text = "\(changeDoubleToMoney(amount: highestExchange * 0.30))"
-        zeroAmountLabel.text = "\(changeDoubleToMoney(amount: highestExchange * 0.15))"
+        midAmountLabel.text = "\(changeDoubleToMoney(amount: highestExchange * 0.05))"
+        lowAmountLabel.text = "\(inputAmount)"
+        zeroAmountLabel.text = "0.00"
         
     }
     
@@ -112,7 +120,58 @@ class GraphViewController: UIViewController {
 
 }
 
+//MARK: - Creating graph subview objects
+extension GraphViewController {
+    
+    func setupGraphSubviews() {
+        
+        addGraphSubviews()
+        positionSubviews()
+        
+    }
 
+    func addGraphSubviews() {
+        
+        usdGraph.backgroundColor = UIColor.blue
+        gbpGraph.backgroundColor = UIColor.blue
+        eurGraph.backgroundColor = UIColor.blue
+        jpyGraph.backgroundColor = UIColor.blue
+        brlGraph.backgroundColor = UIColor.blue
+        graphView.addSubview(usdGraph)
+        graphView.addSubview(gbpGraph)
+        graphView.addSubview(eurGraph)
+        graphView.addSubview(jpyGraph)
+        graphView.addSubview(brlGraph)
+        
+    }
+    
+    func positionSubviews() {
+        
+        usdGraph.frame = CGRect(x: graphView.bounds.width * 0.03, y: graphView.bounds.height, width: graphView.bounds.width * 0.17, height: 0.0)
+        gbpGraph.frame = CGRect(x: graphView.bounds.width * 0.22, y: graphView.bounds.height, width: graphView.bounds.width * 0.17, height: 0.0)
+        eurGraph.frame = CGRect(x: graphView.bounds.width * 0.41, y: graphView.bounds.height, width: graphView.bounds.width * 0.17, height: 0.0)
+        jpyGraph.frame = CGRect(x: graphView.bounds.width * 0.60, y: graphView.bounds.height, width: graphView.bounds.width * 0.17, height: 0.0)
+        brlGraph.frame = CGRect(x: graphView.bounds.width * 0.79, y: graphView.bounds.height, width: graphView.bounds.width * 0.17, height: 0.0)
+        
+    }
+    
+    func animateGraphView() {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            
+            self.usdGraph.frame.size.height += self.graphView.bounds.height * 0.33
+//            self.usdGraph.frame.offsetBy(dx: self.graphView.bounds.width * 0.03, dy: self.graphView.bounds.height * 0.33)
+            
+            self.gbpGraph.frame = CGRect(x: self.graphView.bounds.width * 0.22, y: self.graphView.bounds.height * 0.66, width: self.graphView.bounds.width * 0.17, height: self.graphView.bounds.height * 0.33)
+            self.eurGraph.frame = CGRect(x: self.graphView.bounds.width * 0.41, y: self.graphView.bounds.height * 0.66, width: self.graphView.bounds.width * 0.17, height: self.graphView.bounds.height * 0.33)
+            self.jpyGraph.frame = CGRect(x: self.graphView.bounds.width * 0.60, y: self.graphView.bounds.height * 0.0, width: self.graphView.bounds.width * 0.17, height: self.graphView.bounds.height)
+            self.brlGraph.frame = CGRect(x: self.graphView.bounds.width * 0.79, y: self.graphView.bounds.height * 0.4, width: self.graphView.bounds.width * 0.17, height: self.graphView.bounds.height * 0.6)
+            
+        })
+       
+    }
+    
+}
 
 
 
